b531f8c60c69b4e8f36d40b76b6cdc7c0afb2927
Bohan Huang <weepineapple@gmail.com>
2015-09-18 00:52:53 +0800

    Create test for searchText

--- src/testSearch.java
+++ src/testSearch.java
@@ -10,7 +10,8 @@ public class testSearch {
 	@Test
 	public void test() throws Exception {
 		TextBuddy.initializeTextBuddy("input.txt");
-		String expectedOutcome = "Search result of \"cool\":\n1. cool\n2. super cool\n3. coolll\n";
-		assertEquals(TextBuddy.searchText("cool"), expectedOutcome);
+		String expectedOutcome = "searching result of \"cool\":\n1. cool\n2. super cool\n3. coolll\n";
+		String outcome = TextBuddy.searchText("search cool");
+		assertEquals(outcome, expectedOutcome);
 	}
 }
